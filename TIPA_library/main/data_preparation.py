5cae97fe70040baa10ce9ba1879a0252423d0b20
Deep Neuroscience <youngjun.j.cho@gmail.com>
2019-08-30 19:23:00 +0100

    Add files via upload

--- TIPA_library/main/data_preparation.py
+++ TIPA_library/main/data_preparation.py
@@ -1,1 +1,76 @@
+# TIPA (Thermal Imaging-based Physiological and Affective computing) open-source project
 
+## Author(s): Dr. Youngjun Cho*(Assistant Professor, UCL Computer Science)
+## * http://youngjuncho.com
+
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import widgets
+from TIPA_library.main.thermal_image_processing import optimal_quantization_range as oq_range
+    
+class data_preparation_TIPA_protocol:
+    def __init__(self, file_name, known_height, known_width):
+        self.frame_height = known_height
+        self.frame_width = known_width
+        self.file = np.fromfile(file_name, dtype='uint16')
+        self.file = np.reshape(self.file, [-1, self.frame_height*self.frame_width + 4])#
+        self.file = (np.transpose(self.file).astype('uint16'))
+        
+        self.tracked_matrix=[]
+        self.thermal_matrix = self.get_thermal_matrix(self.file)
+        self.time_stamp = self.get_time_stamp(self.file)
+        self.time_stamp[0]=0
+        
+    def get_thermal_matrix(self, file):
+        # assigning pixels to image-frame matrix 'img_frames'
+        num_frames = np.size(file, 1)
+        file_segment=file[0:self.frame_height*self.frame_width, 0:num_frames]; 
+        img_frames = np.reshape(file_segment[0:self.frame_height*self.frame_width, :], [self.frame_height, self.frame_width, num_frames])
+        #img_frames = np.reshape(file[0:320*240, :], [frame_height, frame_width, num_frames])
+        img_frames = (img_frames - 27315) / 100 # celcius
+        return img_frames
+
+
+    def get_time_stamp(self, file):
+        # getting time array from the last two rows of read-in file
+        pre_time_arr = file[self.frame_height*self.frame_width+2:, :]
+        pre_time_arr_high = (np.round(pre_time_arr[:, :]) & 255) << 8
+        pre_time_arr_low = np.round(pre_time_arr[:, :]) >> 8
+        time_stamp = (pre_time_arr_high + pre_time_arr_low).astype('uint32')
+        new_time_stamp = ((np.round(time_stamp[0, :]) << 16) + time_stamp[1, :]) / 1000
+        return new_time_stamp
+
+
+    # To show thermal 2D sequences interactively
+    def interactive_imshow_cond(self, frame_number):
+        plt.figure(2)
+        plt.imshow(self.thermal_matrix[:,:,frame_number], cmap=plt.get_cmap('hot'))
+        plt.colorbar()
+    #     plt.show()
+    
+    def interactive_imshow_cond2(self, frame_number, thermal_range):
+        plt.figure(2)
+        plt.imshow(self.thermal_matrix[:,:,frame_number], cmap=plt.get_cmap('hot'), vmin=thermal_range[0], vmax=thermal_range[1])
+        plt.colorbar()
+    #     plt.show()
+    
+    def interactive_imshow_cond3(self, frame_number):
+        plt.figure(2)
+        min_T, max_T=oq_range(self.thermal_matrix[:,:,frame_number])
+        plt.imshow(self.thermal_matrix[:,:,frame_number], cmap=plt.get_cmap('hot'), vmin=min_T, vmax=max_T)
+        plt.colorbar()
+    #     plt.show()
+    
+    def interactive_imshow_cond4(self, frame_number):
+        plt.figure(2)
+        plt.imshow(self.tracked_matrix[:,:,frame_number], cmap=plt.get_cmap('hot'))
+        plt.colorbar()
+    #     plt.show()    
+    
+class data_preparation_raw_matrix:
+    def __init__(self, matrix, framerate):
+        self.frame_height = matrix.shape[0]
+        self.frame_width = matrix.shape[1]
+        self.frame_length = matrix.shape[2]
+        self.thermal_matrix = matrix
+        self.time_stamp = np.arange(0,self.frame_length/framerate,1/framerate)
---
+++ TIPA_library/main/thermal_image_processing.py
@@ -0,0 +1,203 @@
+# TIPA (Thermal Imaging-based Physiological and Affective computing) open-source project
+
+## Author(s): Dr. Youngjun Cho*(Assistant Professor, UCL Computer Science)
+## * http://youngjuncho.com
+
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import cv2
+    
+''' Optimal Quantization: Optimal Quantization technique adaptively constructs a color mapping of absolute temperature to improve segmentation, classification and tracking
+
+Reference: Cho, Y., Julier, S. J., Marquardt, N., & Bianchi-Berthouze, N. (2017). Robust tracking of respiratory rate in high-dynamic range scenes using mobile thermal imaging. Biomedical optics express, 8(10), 4480-4503.
+'''
+# output = optimal_quantization(mat, True)  
+def optimal_quantization(t2d_data, print_mode = False):
+    
+    vector_data= np.reshape(t2d_data, t2d_data.shape[0]*t2d_data.shape[1])
+    min_T=np.percentile(vector_data, 2.5)  #2.5 percentile point
+    max_T=np.percentile(vector_data, 97.5) #97.5 percentile point
+
+    t2d_data[np.where(t2d_data > max_T)]=max_T
+    t2d_data[np.where(t2d_data < min_T)]=min_T
+
+    opt_T=min_T
+    count=0
+    while True:
+        count+=1
+        mean_back=np.mean(t2d_data[np.where(t2d_data <= opt_T)])
+        mean_obj=np.mean(t2d_data[np.where(t2d_data > opt_T)])
+        if np.abs(opt_T - (mean_back+mean_obj)/2 )<0.005:
+            break;
+        else:
+            opt_T=(mean_back+mean_obj)/2
+
+    min_T=opt_T
+
+    if print_mode:
+        print("optimal thermal range is [%f, %f]"%(min_T,max_T))
+
+    t2d_data[np.where(t2d_data < min_T)]=min_T
+
+    quantized_t_img=np.zeros((t2d_data.shape[0],t2d_data.shape[1]), np.uint8)
+    for i in range(0, t2d_data.shape[0]):
+        for j in range(0, t2d_data.shape[1]):
+            quantized_t_img[i,j]=255*((t2d_data[i,j]-min_T)/(max_T-min_T))
+
+    return quantized_t_img                     
+                         
+def optimal_quantization_range(t2d_data):
+    
+    vector_data= np.reshape(t2d_data, t2d_data.shape[0]*t2d_data.shape[1])
+    min_T=np.percentile(vector_data, 2.5)  #2.5 percentile point
+    max_T=np.percentile(vector_data, 97.5) #97.5 percentile point
+
+    t2d_data[np.where(t2d_data > max_T)]=max_T
+    t2d_data[np.where(t2d_data < min_T)]=min_T
+
+    opt_T=min_T
+    count=0
+    while True:
+        count+=1
+        mean_back=np.mean(t2d_data[np.where(t2d_data <= opt_T)])
+        mean_obj=np.mean(t2d_data[np.where(t2d_data > opt_T)])
+        if np.abs(opt_T - (mean_back+mean_obj)/2 )<0.005:
+            break;
+        else:
+            opt_T=(mean_back+mean_obj)/2
+
+    min_T=opt_T
+
+    t2d_data[np.where(t2d_data < min_T)]=min_T
+
+    quantized_t_img=np.zeros((t2d_data.shape[0],t2d_data.shape[1]), np.uint8)
+    for i in range(0, t2d_data.shape[0]):
+        for j in range(0, t2d_data.shape[1]):
+            quantized_t_img[i,j]=255*((t2d_data[i,j]-min_T)/(max_T-min_T))
+
+    return min_T,max_T     
+    
+'''
+ This is the classical quantization method using fixed points (temperature range of interest).
+'''
+# output = nonoptimal_quantization(mat, T1,Tk, True)  
+def nonoptimal_quantization(t2d_data, min_T, max_T, print_mode = False):
+    if print_mode:
+        print("your fixed thermal range of interest is [%f, %f]"%(min_T,max_T))
+
+    quantized_t_img=np.zeros((t2d_data.shape[0],t2d_data.shape[1]), np.uint8)
+    
+    t2d_data[np.where(t2d_data > max_T)]=max_T
+    t2d_data[np.where(t2d_data < min_T)]=min_T
+    
+    for i in range(0, t2d_data.shape[0]):
+        for j in range(0, t2d_data.shape[1]):
+            quantized_t_img[i,j]=255*(t2d_data[i,j]-min_T)/(max_T-min_T)
+
+    return quantized_t_img    
+
+
+
+    
+''' Quantization-enabled thermal image tracking
+
+Reference [1]: Cho, Y., Julier, S. J., Marquardt, N., & Bianchi-Berthouze, N. (2017). Robust tracking of respiratory rate in high-dynamic range scenes using mobile thermal imaging. Biomedical optics express, 8(10), 4480-4503.
+Reference [2]: Cho, Y., Julier, S. J., & Bianchi-Berthouze, N. (2019). Instant Stress: Detection of Perceived Mental Stress Through Smartphone Photoplethysmography and Thermal Imaging. JMIR mental health, 6(4), e10140.
+Reference [3]: Cho, Y., Bianchi-Berthouze, N., Oliveira, M., Holloway, C., & Julier, S. (2019). Nose Heat: Exploring Stress-induced Nasal Thermal Variability through Mobile Thermal Imaging. In 2019 eighth International Conference on Affective Computing and Intelligent Interaction (ACII). IEEE.
+'''
+
+## eg. ROI_seq, tracked_img=thermal_tracker(data.thermal_matrix, 'MEDIANFLOW', False)     
+def thermal_tracker(mat_3d, quantization_method='optimal', im_tracker_name = 'TLD', print_mode=False, nonfixed_ROI_size=True, img_viewer=True, min_T=28, max_T=38):
+
+    if im_tracker_name == 'TLD':
+        im_tracker = cv2.TrackerTLD_create()
+    elif im_tracker_name == 'MEDIANFLOW':
+        im_tracker = cv2.TrackerMedianFlow_create()
+    elif im_tracker_name == 'GOTURN':
+        im_tracker = cv2.TrackerGOTURN_create()
+    elif im_tracker_name == 'MOSSE':
+        im_tracker = cv2.TrackerMOSSE_create()
+    elif im_tracker_name == "CSRT":
+        im_tracker = cv2.TrackerCSRT_create()
+    elif im_tracker_name == 'BOOSTING':
+        im_tracker = cv2.TrackerBoosting_create()
+    elif im_tracker_name == 'MIL':
+        im_tracker = cv2.TrackerMIL_create()
+    elif im_tracker_name == 'KCF':
+        im_tracker = cv2.TrackerKCF_create()
+
+
+    temp_mat = copy.deepcopy(mat_3d[:,:,0])    
+    tracked_imgs = np.zeros(mat_3d.shape,np.uint8)
+    
+    if quantization_method == 'optimal':
+        tracked_imgs[:,:,0] = optimal_quantization(temp_mat, print_mode)
+    elif quantization_method == 'non-optimal':
+        tracked_imgs[:,:,0] = nonoptimal_quantization(temp_mat, min_T, max_T, print_mode)
+        
+        
+    # ROI selection 
+    mROI = np.zeros((4, mat_3d.shape[2]))
+    mROI[:,0] = cv2.selectROI('ROI selection', tracked_imgs[:,:,0], False)
+    # mROI.shape
+    cv2.destroyWindow('ROI selection')
+
+#     cv2.namedWindow('TrackViewer')
+    
+    # Initialization of a tracker
+    out = im_tracker.init(tracked_imgs[:,:,0], tuple(mROI[:,0]))
+
+
+    for i in range(1,mat_3d.shape[2]):
+        if np.mod(i,20)==0:
+            print("frame: %d"%i)
+        temp_mat = copy.deepcopy(mat_3d[:,:,i])   
+        
+        if quantization_method == 'optimal':
+            current_frame = optimal_quantization(temp_mat, print_mode)
+        elif quantization_method == 'non-optimal':
+            current_frame = nonoptimal_quantization(temp_mat, min_T, max_T, print_mode)        
+        
+        
+        # Updating the tracker
+        if nonfixed_ROI_size:
+            result, mROI[:,i] = im_tracker.update(current_frame)
+        else:
+            result, bbox = im_tracker.update(current_frame)
+            mROI[2,i]=mROI[2,0]
+            mROI[3,i]=mROI[3,0]
+            mROI[0,i]=bbox[0]+np.round(bbox[2]/2) -np.round(mROI[2,0]/2)
+            mROI[1,i]=bbox[1]+np.round(bbox[3]/2) -np.round(mROI[3,0]/2)
+            
+
+        # Visualise the tracked ROI
+        if result==True:        
+            point1 = (int(mROI[0,i]), int(mROI[1,i]))
+            point2 = (int(mROI[0,i] + mROI[2,i]), int(mROI[1,i] + mROI[3,i]))
+            
+            cv2.rectangle(current_frame, point1, point2, (255,255,0), 3, 0)
+            tracked_imgs[:,:,i]=current_frame
+        else :
+            print("tracking error occurred at %d frame"%(i))
+            tracked_imgs[:,:,i]=current_frame
+
+        if img_viewer:
+            cv2.imshow('TrackViewer',current_frame)
+                    # Exit if ESC pressed
+            k = cv2.waitKey(1) & 0xff
+            if k == 27 : break
+
+#     cv2.namedWindow('TrackViewer')
+#     count=0
+#     while True:
+#         cv2.imshow('TrackViewer',tracked_imgs[:,:,count])
+#         if count<tracked_imgs.shape[2]-1:
+#             count+=1
+#         # Press ESC to exit
+#         k = cv2.waitKey(1) & 0xff
+#         if k == 27 : break
+#     cv2.destroyAllWindows()
+
+            
+    return mROI, tracked_imgs
